a75885f6d754647f4d0840fc7f964ec4364e82ad
evan <evanbonner99@gmail.com>
2023-07-12 16:22:23 +0100

    Update build.zig

--- build.zig
+++ build.zig
@@ -9,16 +9,7 @@ pub fn build(b: *std.Build) void {
     _ = b.addModule("grapheme", .{
         .source_file = .{ .path = "src/main.zig" },
     });
-
-    // Hacky way to avoid having to expose a custom `link` function to users of this library
-    const lib = b.addStaticLibrary(.{
-        .name = "grapheme-zig",
-        .root_source_file = .{ .path = "stub.c" },
-        .optimize = optimize,
-        .target = target,
-    });
-    lib.linkLibrary(grapheme.artifact("grapheme"));
-    b.installArtifact(lib);
+    b.installArtifact(grapheme.artifact("grapheme"));
 
     const tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
