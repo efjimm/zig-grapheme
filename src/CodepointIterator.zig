1017b805ecf392791d09d5b4febecc984262a85b
evan <evanbonner99@gmail.com>
2023-07-12 14:56:11 +0100

    Make codepoint.zig use u32 instead of u21 + more
    Also adds lowerase checking and conversion for codepoints.
    
    Previously we used u21 arrays and casted them to the u32 arrays that
    grapheme expects. u21s in zig get padded to the next power of two, so
    while the size was correct the value of the padding bytes is undefined.
    For this reason the API now uses u32s for codepoint pointers. If you
    have pointers to u21s and can ensure that the padding bits for each
    codepoint are zero, then you can just cast them to a u32 slice to use
    these functions.

--- src/CodepointIterator.zig
+++ src/CodepointIterator.zig
@@ -2,29 +2,29 @@ const std = @import("std");
 const cp = @import("codepoint.zig");
 const Self = @This();
 
-str: []const u21,
+str: []const u32,
 index: usize = 0,
 
-fn next(self: *Self, comptime func: fn ([]const u21) usize) ?[]const u21 {
+fn next(self: *Self, comptime func: fn ([]const u32) usize) ?[]const u32 {
     if (self.index >= self.str.len) return null;
     const start = self.index;
     self.index += func(self.str[start..]);
     return self.str[start..self.index];
 }
 
-pub fn nextGrapheme(self: *Self) ?[]const u21 {
+pub fn nextGrapheme(self: *Self) ?[]const u32 {
     return self.next(cp.nextCharacterBreak);
 }
 
-pub fn nextLineBreak(self: *Self) ?[]const u21 {
+pub fn nextLineBreak(self: *Self) ?[]const u32 {
     return self.next(cp.nextLineBreak);
 }
 
-pub fn nextSentenceBreak(self: *Self) ?[]const u21 {
+pub fn nextSentenceBreak(self: *Self) ?[]const u32 {
     return self.next(cp.nextSentenceBreak);
 }
 
-pub fn nextWordBreak(self: *Self) ?[]const u21 {
+pub fn nextWordBreak(self: *Self) ?[]const u32 {
     return self.next(cp.nextWordBreak);
 }
 
--- src/codepoint.zig
+++ src/codepoint.zig
@@ -5,7 +5,7 @@ const c = @cImport({
 const utf8 = @import("utf8.zig");
 
 comptime {
-    std.debug.assert(@sizeOf(c.uint_least32_t) == @sizeOf(u21));
+    std.debug.assert(@sizeOf(c.uint_least32_t) == @sizeOf(u32));
     std.debug.assert(@sizeOf(c.uint_least16_t) == @sizeOf(u16));
 }
 
@@ -17,7 +17,7 @@ pub const Iterator = @import("CodepointIterator.zig");
 ///
 /// For UTF-8-encoded input data `utf8.nextCharacterBreak` can be used
 /// instead.
-pub fn nextCharacterBreak(str: []const u21) usize {
+pub fn nextCharacterBreak(str: []const u32) usize {
     return c.grapheme_next_character_break(@ptrCast(str.ptr), str.len);
 }
 
@@ -25,7 +25,7 @@ pub fn nextCharacterBreak(str: []const u21) usize {
 /// codepoint slice `str`.
 ///
 /// For UTF-8-encoded input data `utf8.nextLineBreak` can be used instead.
-pub fn nextLineBreak(str: []const u21) usize {
+pub fn nextLineBreak(str: []const u32) usize {
     return c.grapheme_next_line_break(@ptrCast(str.ptr), str.len);
 }
 
@@ -34,7 +34,7 @@ pub fn nextLineBreak(str: []const u21) usize {
 /// to the length of said sentence.
 ///
 /// For UTF-8-encoded input data `utf8.nextSentenceBreak` can be used instead
-pub fn nextSentenceBreak(str: []const u21) usize {
+pub fn nextSentenceBreak(str: []const u32) usize {
     return c.grapheme_next_sentence_break(@ptrCast(str.ptr), str.len);
 }
 
@@ -43,8 +43,44 @@ pub fn nextSentenceBreak(str: []const u21) usize {
 /// of said word.
 ///
 /// For UTF-8-encoded input data `utf8.nextWordBreak` can be used instead.
-pub fn nextWordBreak(str: []const u21) usize {
+pub fn nextWordBreak(str: []const u32) usize {
     return c.grapheme_next_word_break(@ptrCast(str.ptr), str.len);
+}
+
+/// Returns the offset (in codepoints) to the next grapheme_cluster break in
+/// the null-terminated codepoint pointer `str`. If a grapheme cluster begins
+/// at `str` this offset is equal to the length of said grapheme cluster.
+///
+/// For UTF-8-encoded input data `utf8.nextCharacterBreakZ` can be used
+/// instead.
+pub fn nextCharacterBreakZ(str: [*:0]const u32) usize {
+    return c.grapheme_next_character_break(@ptrCast(str), c.SIZE_MAX);
+}
+
+/// Returns the offset (in codepoints) to the next possible line break in the
+/// null-terminated codepoint pointer `str`.
+///
+/// For UTF-8-encoded input data `utf8.nextLineBreakZ` can be used instead.
+pub fn nextLineBreakZ(str: [*:0]const u32) usize {
+    return c.grapheme_next_line_break(@ptrCast(str), c.SIZE_MAX);
+}
+
+/// Returns the offset (in codepoints) to the next sentence break in the
+/// null-terminated codepoint pointer `str`. If a sentence begins at `str` this
+/// offset is equal to the length of said sentence.
+///
+/// For UTF-8-encoded input data `utf8.nextSentenceBreakZ` can be used instead
+pub fn nextSentenceBreakZ(str: [*:0]const u32) usize {
+    return c.grapheme_next_sentence_break(@ptrCast(str), c.SIZE_MAX);
+}
+
+/// Returns the offset (in codepoints) to the next word break in the
+/// null-terminated codepoint pointer `str`. If a word begins at `str` this
+/// offset is equal to the length of said word.
+///
+/// For UTF-8-encoded input data `utf8.nextWordBreakZ` can be used instead.
+pub fn nextWordBreakZ(str: [*:0]const u32) usize {
+    return c.grapheme_next_word_break(@ptrCast(str), c.SIZE_MAX);
 }
 
 /// Determines if there is a grapheme cluster break between the two codepoints
@@ -54,6 +90,83 @@ pub fn nextWordBreak(str: []const u21) usize {
 ///
 /// If `state` is null `isCharacterBreak` behaves as if it was called with a
 /// fully reset state.
-pub fn isCharacterBreak(cp1: u21, cp2: u21, state: ?*u16) bool {
+pub fn isCharacterBreak(cp1: u32, cp2: u32, state: ?*u16) bool {
     return c.grapheme_is_character_break(cp1, cp2, state);
+}
+
+/// Checks if the codepoint slice `str` is lowercase.
+///
+/// For UTF-8-encoded input data `utf8.isLowercase` can be used instead.
+/// For null-terminated codepoint pointers `isLowercaseZ` can be used instead.
+pub fn isLowercase(str: []const u32) bool {
+    return c.grapheme_is_lowercase(@ptrCast(str.ptr), str.len, null);
+}
+
+/// Checks if the null-terminated codepoint pointer `str` is lowercase.
+///
+/// For UTF-8-encoded input data `utf8.isLowercaseZ` can be used instead.
+pub fn isLowercaseZ(str: [*:0]const u32) bool {
+    return c.grapheme_is_lowercase(@ptrCast(str), c.SIZE_MAX, null);
+}
+
+/// Checks if the codepoint slice `str` is lowercase and also returns the
+/// length of the matching lowercase sequence.
+///
+/// For UTF-8-encoded input data `utf8.isLowercaseLen` can be used instead.
+/// For null-terminated codepoint pointers see `isLowercaseLenZ`.
+pub fn isLowercaseLen(str: []const u32) struct {
+    is_lowercase: bool,
+    case_len: usize,
+} {
+    var case_len: usize = undefined;
+    const is_lowercase = c.grapheme_is_lowercase(@ptrCast(str.ptr), str.len, &case_len);
+    return .{
+        .is_lowercase = is_lowercase,
+        .case_len = case_len,
+    };
+}
+
+/// Checks if the null-terminated codepoint pointer `str` is lowercase and
+/// also returns the length of the matching lowercase sequence.
+///
+/// For UTF-8-encoded input data `utf8.isLowercaseLen` can be used instead.
+pub fn isLowercaseLenZ(str: [*:0]const u32) struct {
+    is_lowercase: bool,
+    case_len: usize,
+} {
+    var case_len: usize = undefined;
+    const is_lowercase = c.grapheme_is_lowercase(@ptrCast(str), c.SIZE_MAX, &case_len);
+    return .{
+        .is_lowercase = is_lowercase,
+        .case_len = case_len,
+    };
+}
+
+/// Converts the codepoint slice `str` to lowercase and writes the result to
+/// `dest`. `dest.len` need not be greater than `src.len`. Returns the number
+/// of codepoints in the slice resulting from converting `src` to lowercase,
+/// even if `dest` is not large enough.
+///
+/// For UTF-8-encoded data `utf8.toLowercase` can be used instead.
+/// For null-terminated codepoint pointers see `toLowercaseZ`.
+pub fn toLowercase(src: []const u32, dest: []u32) usize {
+    return c.grapheme_to_lowercase(@ptrCast(src.ptr), src.len, @ptrCast(dest.ptr), dest.len);
+}
+
+/// Returns the number of codepoints required to convert `src` to lowercase.
+pub fn toLowercaseLen(src: []const u32) usize {
+    return c.grapheme_to_lowercase(@ptrCast(src.ptr), src.len, null, 0);
+}
+
+/// Converts the codepoint slice `str` to lowercase and writes the result to
+/// `dest`. `dest.len` need not be greater than `src.len`.
+///
+/// For UTF-8-encoded data `utf8.toLowercase` can be used instead.
+pub fn toLowercaseZ(src: [*:0]const u32, dest: []u32) usize {
+    return c.grapheme_to_lowercase(@ptr(src), c.SIZE_MAX, @ptrCast(dest.ptr), dest.len);
+}
+
+/// Returns the number of codepoints required to convert `src` to lowercase.
+pub fn toLowercaseLenZ(src: [*:0]const u32) usize {
+    return c.grapheme_to_lowercase(@ptrCast(ptr), c.SIZE_MAX, null, 0);
 }
